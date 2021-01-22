21556599483bc5ca0fcb5942f5232f38832d6f4f
Sinem Babuçcu <babuccusinem@gmail.com>
2021-01-22 23:10:20 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //typw safety - tip güvenliği. bu kısım yorum satırıdır, görünmez
+
+            string kategoriEtiketi = "Kategori";
+
+            Console.WriteLine(kategoriEtiketi);
+
+
+            Console.WriteLine(kategoriEtiketi);
+
+            Console.WriteLine(kategoriEtiketi);
+
+            Console.WriteLine(kategoriEtiketi);
+
+   
